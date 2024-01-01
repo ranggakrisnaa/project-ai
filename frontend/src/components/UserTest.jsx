aeb98216552edd3f95d0bf6cd4e12188d6eb74b6
ranggakrisnaa <infinity.duos@gmail.com>
2024-01-01 20:21:09 +0700

    add more file

--- frontend/src/components/UserTest.jsx
+++ frontend/src/components/UserTest.jsx
@@ -5,7 +5,7 @@ import { Image } from '@chakra-ui/image'
 const UserTest = () => {
     return (
         <Flex flexWrap={'wrap'} mt={'40px'} gap={'20px'}>
-            <Box display={'flex'} gap={'20px'}>
+            <Flex gap={'20px'} flexBasis={0}>
                 <Box w={'370px'} h={'164px'} bgColor={'#FFFFFF'} p={'16px'} rounded={'32px'}>
                     <Text fontWeight={'light'} fontSize={'18px'} ml={'10px'} fontStyle={'italic'}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
@@ -30,8 +30,8 @@ const UserTest = () => {
                         </Box>
                     </Flex>
                 </Box>
-            </Box>
-            <Flex gap={'20px'}>
+            </Flex>
+            <Flex gap={'20px'} flexBasis={0}>
                 <Box w={'370px'} h={'164px'} bgColor={'#FFFFFF'} p={'16px'} rounded={'32px'}>
                     <Text fontWeight={'light'} fontSize={'18px'} ml={'10px'} fontStyle={'italic'}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
